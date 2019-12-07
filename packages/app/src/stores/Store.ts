dbdb7204742b4f1790d744c52be537ea76969ca9
bobinlee <bobeen.lee@huiseoul.com>
2019-12-07 20:37:02 +0900

    change naming

--- packages/app/src/stores/Store.ts
+++ packages/app/src/stores/Store.ts
@@ -1,7 +1,7 @@
 import { types } from "mobx-state-tree";
 
 const Store = types.model({
-  text: types.optional(types.string, "FitsMe")
+  text: types.optional(types.string, "RNApp")
 });
 
 export type IStore = typeof Store.Type;
