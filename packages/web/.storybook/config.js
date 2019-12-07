a1cc302dd7b8661a56b880cf30b9ab9d0b45f501
bobinlee <bobeen.lee@huiseoul.com>
2019-12-07 21:32:45 +0900

    코드 수정

--- packages/web/.storybook/config.js
+++ packages/web/.storybook/config.js
@@ -3,8 +3,6 @@ import { configure, addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { createGlobalStyle } from "styled-components";
 
-import 'antd/dist/antd.css';
-
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /.stories.tsx$/);
 function loadStories() {
