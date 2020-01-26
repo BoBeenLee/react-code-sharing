b74aa6d9942c8a2197477284622cb6604972ffc0
bobinlee <bobeen.lee@huiseoul.com>
2020-01-26 19:00:40 +0900

    [chore] remove 불필요코드

--- packages/web/src/styles/device.ts
+++
@@ -1,9 +0,0 @@
-const size = {
-  mobileM: "360px",
-  tablet: "768px"
-};
-
-export const device = {
-  mobileM: `(min-width: ${size.mobileM})`,
-  tablet: `(min-width: ${size.tablet})`
-};
--- packages/web/src/styles/zIndexes.ts
+++ packages/web/src/styles/zIndexes.ts
@@ -1,4 +1,3 @@
-// tslint:disable:object-literal-sort-keys
 const zIndex = {
   one: 1,
   ten: 10,
