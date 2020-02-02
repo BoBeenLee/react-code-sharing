8fe6c792e4c188218bc20dff217ff4c57a364f69
bobinlee <bobeen.lee@huiseoul.com>
2020-02-03 04:33:06 +0900

    [style] fix global storybook style

--- packages/web/.storybook/config.js
+++ packages/web/.storybook/config.js
@@ -9,8 +9,8 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 global.___loader = {
-  enqueue: () => { },
-  hovering: () => { }
+  enqueue: () => {},
+  hovering: () => {}
 };
 // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
 global.__PATH_PREFIX__ = "";
@@ -23,9 +23,22 @@ const GlobalStyle = createGlobalStyle`
   @import url("https://fonts.googleapis.com/css?family=Heebo:400,700|Roboto:400,700&display=swap");
   @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
   
-  * {
-    font-family: 'Spoqa Han Sans', 'Roboto';
-  }
+  *, html, body {
+  font-family: "NanumSquare", sans-serif;
+  font-size: 16px;
+  outline: none;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  word-break: keep-all;
+}
+
+html, body, #root {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  height: 100vh;
+  width: 100vw;
+}
 `;
 
 addDecorator(story => (
