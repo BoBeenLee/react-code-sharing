01c484b814d12b22ebd0e38a966e5d192cd943e2
bobinlee <bobeen.lee@huiseoul.com>
2020-02-04 18:41:56 +0900

    [fix] 테스트 환경 수정

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,15 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require(`path`);
 
 module.exports = {
+  setupFilesAfterEnv: ["<rootDir>src/setupTests.js"],
   moduleDirectories: ["node_modules", path.resolve(__dirname)],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`
   },
+  testRegex: "(.*(test|spec))\\.(jsx?|tsx?)$",
   testPathIgnorePatterns: [`node_modules`, `.cache`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
-    __PATH_PREFIX__: ``,
+    "ts-jest": {
+      babelConfig: ".babelrc"
+    },
+    __PATH_PREFIX__: ``
   },
-  testURL: `http://localhost`,
+  testURL: `http://localhost`
 };
