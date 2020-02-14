bf138eb5f62a97586733993206b51ed93f086a0c
bobinlee <bobeen.lee@huiseoul.com>
2020-02-14 20:19:54 +0900

    [desktop] config babel, webpack

--- packages/desktop/webpack.rules.js
+++ packages/desktop/webpack.rules.js
@@ -2,26 +2,23 @@ module.exports = [
   // Add support for native node modules
   {
     test: /\.node$/,
-    use: 'node-loader',
+    use: "node-loader"
   },
   {
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      loader: "@marshallofsound/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
-      },
-    },
-  },
-  {
-    test: /\.tsx?$/,
-    exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: 'ts-loader',
-      options: {
-        transpileOnly: true
+        outputAssetBase: "native_modules"
       }
     }
   },
+  {
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve("babel-loader"),
+    options: {
+      presets: [["react-app", { flow: false, typescript: true }]]
+    }
+  }
 ];
