2c184a5ce4c5057101c411c81224f8f928b159c8
bobinlee <bobeen.lee@huiseoul.com>
2020-07-14 09:25:52 +0900

    feat: upgrade firebase v6

--- packages/app/react-native.config.js
+++ packages/app/react-native.config.js
@@ -1,33 +1,27 @@
 module.exports = {
-    assets: ['./assets/fonts/'],
-    dependencies: {
-        'react-native-navigation': {
-            platforms: {
-                ios: null
-            },
-        },
-        'react-native-firebase': {
-            platforms: {
-                ios: null,
-                android: null, // disable Android platform, other platforms will still autolink if provided
-            },
-        },
-        'lottie-react-native': {
-            platforms: {
-                android: null, // disable Android platform, other platforms will still autolink if provided
-            },
-        },
-        'react-native-code-push': {
-            platforms: {
-                ios: null,
-                android: null, // disable Android platform, other platforms will still autolink if provided
-            }
-        },
-        'rn-fetch-blob': {
-            platforms: {
-                ios: null,
-                android: null, // disable Android platform, other platforms will still autolink if provided
-            }
-        }
+  assets: ["./assets/fonts/"],
+  dependencies: {
+    "react-native-navigation": {
+      platforms: {
+        ios: null
+      }
     },
+    "lottie-react-native": {
+      platforms: {
+        android: null // disable Android platform, other platforms will still autolink if provided
+      }
+    },
+    "react-native-code-push": {
+      platforms: {
+        ios: null,
+        android: null // disable Android platform, other platforms will still autolink if provided
+      }
+    },
+    "rn-fetch-blob": {
+      platforms: {
+        ios: null,
+        android: null // disable Android platform, other platforms will still autolink if provided
+      }
+    }
+  }
 };
